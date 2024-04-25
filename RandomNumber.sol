965332f2fa08b7f62b56d6266c8e652f76fe14d1
swayam karle <devswayam@swayams-MacBook-Pro.local>
2024-04-26 02:12:47 +0400

    Working Cross Chain Calls

--- RandomNumber.sol
+++ RandomNumber.sol
@@ -1,44 +1,57 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
+
+/* 
+This contract is an example contract to demonstrate 
+the cross-chain function call using ExecuteAPI 
+on the inco chain.
+*/
+
 pragma solidity >=0.8.13 <0.9.0;
 
 import "fhevm/abstracts/EIP712WithModifier.sol";
 import "fhevm/lib/TFHE.sol";
 
 interface IInterchainExecuteRouter {
-    function getRemoteInterchainAccount(uint32 _destination, address _owner) external view returns (address);
+
+    function getRemoteInterchainAccount(uint32 _destination, address _owner)
+        external
+        view
+        returns (address);
+
 }
 
-contract RandomNumber is EIP712WithModifier {
-    uint32 ChainID; 
-    address public iexRouter; 
+interface IRandomNumber {
+    function returnNumber(address user) external returns(uint16);
+}
+
+contract RandomNumber is EIP712WithModifier,IRandomNumber {
+
+    mapping (address => euint16) public encryptedNumbers;
+    uint32 DestinationDomain;
+    address public iexRouter;
     address public caller_contract;
-    
+
     constructor() EIP712WithModifier("Authorization token", "1") {
-        ChainID = 17001;
-        iexRouter = 0x4Bc0b9BD1d285F10AE69cef5B8ACbeaf1d28D71B;
     }
-    
-    function setCallerContract(address _caller_contract) public {
+
+    function initialize(uint32 _DestinationDomain, address _caller_contract, address _iexRouter) public {
+        DestinationDomain = _DestinationDomain;
+        iexRouter = _iexRouter;
         caller_contract = _caller_contract;
     }
 
-    function getICA() public view returns(address) {
-        return IInterchainExecuteRouter(iexRouter).getRemoteInterchainAccount(ChainID, address(this));
-    }
-    
-    modifier onlyCallerContract() {
+    // A random encrypted uint8 is generated
+    function returnNumber(address user) external returns(uint16) {
         require(caller_contract == msg.sender, "not right caller contract");
-        _;
-    }
-    
-    mapping (address => euint16) public encryptedNumbers;
-
-    function returnNumber(address user) external onlyCallerContract returns(uint16) {
-        encryptedNumbers[user] = TFHE.rem(TFHE.randEuint16(), 100);
+        encryptedNumbers[user] = TFHE.randEuint16();
         return TFHE.decrypt(encryptedNumbers[user]);
     }
 
-    function viewNumber(address user) external returns (uint16) {
-        return TFHE.decrypt(encryptedNumbers[user]);
+    //function decryptRandomNumber(address user) external returns (uint16) {
+    //    return TFHE.decrypt(encryptedNumbers[user]);
+    //}
+
+    function getICA(address _contract) public view returns(address) {
+        return IInterchainExecuteRouter(iexRouter).getRemoteInterchainAccount(DestinationDomain, _contract);
     }
 }
--- SlotMachine.sol
+++ SlotMachine.sol
@@ -1,71 +1,77 @@
-// SPDX-License-Identifier: BSD-3-Clause-Clear
-// @Dev this contract is for non inco network 
+// SPDX-License-Identifier: Apache-2.0
+
+/* 
+This contract is an example contract to demonstrate 
+the cross-chain function call using ExcuteAPI 
+on the base chain.
+*/
 
 pragma solidity >=0.8.13 <0.9.0;
 
 interface IInterchainExecuteRouter {
-    function getRemoteInterchainAccount(uint32 _destination, address _owner) external view returns (address);
-    // New
-    function callRemote(uint32 _destination, address _to, uint256 _value, bytes calldata _data, bytes memory _callback) external returns (bytes32);
+
+    function callRemote(
+        uint32 _destination,
+        address _to,
+        uint256 _value,
+        bytes calldata _data,
+        bytes memory _callback
+    ) external returns (bytes32);
+
+    function getRemoteInterchainAccount(uint32 _destination, address _owner)
+        external
+        view
+        returns (address);
+
 }
 
 interface RandomNumber {
-    function returnNumber(address user) external returns(uint16);
+    function returnNumber(address user) external returns(uint16) ;
 }
 
 contract SlotMachine {
-
-    uint32 ChainID; // Inco Chain Id = 9090
-    address rngContract; // Random Number Generator contract on Inco Network = 0xC3180fe621A63Fb21EfCF66FA2253CBc28CC834C
-    address iexRouter; // 
-    address caller_contract;
-    bool public isInitialized;
-    uint16 lastRandomNumber;
-
-    constructor(){
-        ChainID = 9090;
-        iexRouter = 0xAa3a222f42D034BC45a732827888e2C152591592;
-        caller_contract = 0xAa3a222f42D034BC45a732827888e2C152591592;
-    }
-
-    function initialize( address _rngContract) public {
-        require(isInitialized == false, "Bridge contract already initialized");
-        rngContract = _rngContract; 
-        isInitialized = true;
-    }
     
-    function setCallerContract(address _caller_contract) onlyCallerContract public {
+    uint32 DestinationDomain;
+    // HiddenCard contract in Inco Network
+    address randomNumber;
+    // InterchainExcuteRouter contract address in current chain
+    address iexRouter;
+    address caller_contract;
+    bytes32 messageId;
+    mapping (address => uint16) public randomNumbers;
+
+    function initialize(uint32 _DestinationDomain, address _randomNumber, address _iexRouter, address _caller_contract) public {
+        DestinationDomain = _DestinationDomain;
+        randomNumber = _randomNumber;
+        iexRouter = _iexRouter;
         caller_contract = _caller_contract;
     }
 
-    function getICA() public view returns(address) {
-        return IInterchainExecuteRouter(iexRouter).getRemoteInterchainAccount(ChainID, address(this));
-    }
-    
-    modifier onlyCallerContract() {
-        require(caller_contract == msg.sender, "not right caller contract");
-        _;
-    }
+    function GetNumber(address user) public {
+        RandomNumber _randomNumber = RandomNumber(randomNumber);
 
-    function getRandomNumber(address user) public {
-        RandomNumber _rng = RandomNumber(rngContract);
+        bytes memory _callback = abi.encodePacked(this.spinSlotMachine.selector,(uint256(uint160(user))));
 
-        bytes memory _callback = abi.encodePacked(this.spin.selector, (uint256(uint160(msg.sender))));
-
-        IInterchainExecuteRouter(iexRouter).callRemote(
-            ChainID,
-            address(_rng),
+        messageId = IInterchainExecuteRouter(iexRouter).callRemote(
+            DestinationDomain,
+            address(_randomNumber),
             0,
-            abi.encodeCall(_rng.returnNumber, (user)),
+            abi.encodeCall(_randomNumber.returnNumber,(user)),
             _callback
         );
     }
 
-    function spin(uint256 user,uint16 _rndNumber) onlyCallerContract external {
-        lastRandomNumber = _rndNumber;
+    function spinSlotMachine(uint256 user, uint16 _rn) external {
+        require(caller_contract == msg.sender, "not right caller contract"); // ?
+        randomNumbers[address(uint160(user))] = _rn;
     }
-    
-    function randomNumberView() public view returns(uint256) {
-        return lastRandomNumber;
+
+    function ViewSlotMachineNumber(address user) public view returns(uint16) {
+        return randomNumbers[user];
     }
+
+    function getICA(address _contract) public view returns(address) {
+        return IInterchainExecuteRouter(iexRouter).getRemoteInterchainAccount(DestinationDomain, _contract);
+    }
+
 }
